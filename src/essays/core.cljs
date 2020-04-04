4ff4355f3f875eff22e662c0b06ba08742d6ade1
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-04-03 22:52:33 -0400

    Renamed to Essays to reflect change of focus for website

---
+++ src/essays/core.cljs
@@ -0,0 +1,21 @@
+(ns essays.core
+  (:require [reagent.core :as r]
+            [reagent.dom :as rdom]
+            [clojure.string :as str]))
+
+
+(defn state (atom {:title "Essays"
+                   :author "Bardia"}))
+
+
+(defn app []
+  [:div
+   [:h1 (:title @state)]
+   [:h2 "By " (:author @state)]])
+
+(defn ^:dev/after-load start []
+  (rdom/render [app] (js/document.getElementById "app")))
+
+(defn init []
+  (js/console.log "init")
+  (start))
